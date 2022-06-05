150a1a28a43c523d7b4e603be546ac885d3b836b
Sihkdragon <dithor37@gmail.com>
2022-06-05 21:46:19 +0700

    try to add responsive

--- src/assets/icon/index.js
+++ src/assets/icon/index.js
@@ -2,5 +2,5 @@ import phone from "./phone.svg";
 import email from "./email.svg";
 import instagram from "./ig.svg";
 import web from "./web.svg";
-
-export { phone, email, instagram, web };
+import menuIcon from "./menu.svg";
+export { phone, email, instagram, web, menuIcon };
--- src/components/molecules/RightNavigation.js
+++ src/components/molecules/RightNavigation.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { menu } from "../../lib";
+import { menuIcon } from "../../assets/icon";
 const RightNavigation = () => {
   return (
     <ul className="flex text-xl capitalize text-white gap-x-4">
+      <button className="w-fit lg:hidden mr-4">
+        <img src={menuIcon} alt="menuIcon" />
+      </button>
       {menu.map((menu) => {
         return (
-          <li className="" key={menu.id}>
+          <li className="hidden lg:block" key={menu.id}>
             <a href={`/${menu.link}`}>{menu.name}</a>
           </li>
         );
